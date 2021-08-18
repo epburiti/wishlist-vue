f4cba10d3569a42b5d2a71d8b8419fe5eb967ac1
epburiti <epburiti>
2021-08-17 21:12:53 -0300

    basic config

---
+++ src/components/Header/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>MegaNets</h1>
+    <div class="content-search">
+      <ul>
+        <li>Cidade</li>
+        <li>Central de Atendimento</li>
+        <li>Lista de desejos</li>
+      </ul>
+      <input type="text" placeholder="Buscar..." />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./style.scss";
+</style>
